ba0ab7b7e14aba4e02f808d02de9d9c851d63679
Alan <socyware@gmail.com>
2022-02-23 23:37:06 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">
+            App desenvolvido por{" "}
+            <a
+              href="https://github.com/Socyware"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Alan de Souza
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'assets/css/style.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "assets/css/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
